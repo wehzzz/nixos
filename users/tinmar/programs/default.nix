348be01a0bb64e40cb502b77f3aff89027bc6374
Martin Levesque <martin.levesque@epita.fr>
2023-08-02 00:00:23 +0200

    [config #fix] split config files

--- users/tinmar/programs/default.nix
+++ users/tinmar/programs/default.nix
@@ -1,18 +1,6 @@
-let
-  more = { pkgs, ... }: {
-    programs = {
-      htop = {
-        enable = true;
-        settings = {
-          sort_direction = true;
-          sort_key = "PERCENT_CPU";
-        };
-      };
-      ssh.enable = true;
-    };
-  };
-in
-[
-  ./alacritty
-  more
-]
+{...}: {
+  imports = [
+    ./alacritty
+  ]
+
+}

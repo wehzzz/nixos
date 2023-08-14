079340776721e4f5769ba3874803fff96b9dd993
Martin Levesque <martin.levesque@epita.fr>
2023-08-14 15:50:42 +0200

    [rofi] add new config

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -1,10 +1,13 @@
 {pkgs, ...}: {
   programs.rofi = {
     enable = true;
-    terminal = "alacritty";
+    terminal = "${pkgs.alacritty}/bin/alacritty";
   };
 
   home.file = {
-    ".config/networkmanager-dmenu/config.ini".source = ../../programs/polybar/scripts/networkmanager.ini;
+    ".config/networkmanager-dmenu/config.ini".source = ../../programs/rofi/networkmanager.ini;
+    ".config/rofi/colors.rasi".source = ../../programs/rofi/colors.rasi;
+    ".config/rofi/fonts.rasi".source = ../../programs/rofi/fonts.rasi;
+    ".config/rofi/launcher.rasi".source = ../../programs/rofi/launcher.rasi;
   };
 }

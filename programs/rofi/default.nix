ba5e0a86cfeee6d7f7706dafbc03dfe1ae474b82
Martin Levesque <martin.levesque@epita.fr>
2023-08-16 00:45:19 +0200

    [rofi] bluetooth

--- programs/rofi/default.nix
+++ programs/rofi/default.nix
@@ -11,5 +11,7 @@
     ".config/rofi/fonts.rasi".source = ../../programs/rofi/fonts.rasi;
     ".config/rofi/launcher.rasi".source = ../../programs/rofi/launcher.rasi;
     ".config/rofi/powermenu.rasi".source = ../../programs/rofi/powermenu.rasi;
+    ".config/rofi/network.rasi".source = ../../programs/rofi/network.rasi;
+    ".config/rofi/bluetooth.rasi".source = ../../programs/rofi/bluetooth.rasi;
   };
 }
